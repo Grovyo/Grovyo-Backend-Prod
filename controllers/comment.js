af757f8415d0958e8f8f97353aaa4d3ce8df0b32
Shreyansh Singh <63132919+krishna9singh@users.noreply.github.com>
2023-12-20 18:02:23 +0530

    2
    2

--- controllers/comment.js
+++ controllers/comment.js
@@ -160,7 +160,14 @@ exports.fetchallcomments = async (req, res) => {
         );
         dps.push(a);
       }
-      res.status(200).json({ success: true, comment, dps, liked });
+
+      //merging all the data
+      const merged = dps?.map((d, i) => ({
+        dp,
+        comments: comment[i],
+        likes: liked[i],
+      }));
+      res.status(200).json({ success: true, merged });
     }
   } catch (e) {
     res.status(400).json({ message: e.message, success: false });
