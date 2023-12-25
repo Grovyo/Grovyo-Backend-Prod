ae86deedc1b32ef70ef7cb7f35f541114a006132
Shreyansh Singh <63132919+krishna9singh@users.noreply.github.com>
2023-12-25 13:06:35 +0530

    2
    2

--- controllers/post.js
+++ controllers/post.js
@@ -705,7 +705,7 @@ exports.joinedcom = async (req, res) => {
       .populate("creator", "fullname");
 
     if (!communities || communities.length === 0) {
-      res.status(404).json({ message: "No communities found", success: true });
+      res.status(203).json({ message: "No communities found", success: true });
       return;
     }
 
