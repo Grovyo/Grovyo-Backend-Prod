a7c23924311c815798545e6b780003e43cc2b1f4
Shreyansh Singh <63132919+krishna9singh@users.noreply.github.com>
2024-04-14 16:37:25 +0530

    1

--- controllers/userAuth.js
+++ controllers/userAuth.js
@@ -3381,25 +3381,25 @@ exports.usl = async (req, res) => {
   }
 };
 
-const findUser = async () => {
-  try {
-    const users = await User.find();
-    const reverseusers = users.reverse();
-    let data = [];
-    const onlyseven = reverseusers.slice(0, 7);
-    for (let i = 0; i < onlyseven.length; i++) {
-      const d = {
-        fullname: onlyseven[i].fullname,
-        id: onlyseven[i]._id,
-        email: onlyseven[i].email,
-        phone: onlyseven[i].phone,
-      };
-      data.push(d);
-    }
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const findUser = async () => {
+//   try {
+//     const users = await User.find();
+//     const reverseusers = users.reverse();
+//     let data = [];
+//     const onlyseven = reverseusers.slice(0, 7);
+//     for (let i = 0; i < onlyseven.length; i++) {
+//       const d = {
+//         fullname: onlyseven[i].fullname,
+//         id: onlyseven[i]._id,
+//         email: onlyseven[i].email,
+//         phone: onlyseven[i].phone,
+//       };
+//       data.push(d);
+//     }
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-findUser();
+// findUser();
