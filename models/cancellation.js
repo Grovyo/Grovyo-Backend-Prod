2719f7bc70a0b94c256f389144ca813632b78a87
Shreyansh Singh <63132919+krishna9singh@users.noreply.github.com>
2024-07-10 00:52:08 +0530

    1

--- models/cancellation.js
+++ models/cancellation.js
@@ -1,9 +1,16 @@
-const mongoose = require("mongoose")
-const Cancellation = new mongoose.Schema({
-	userid: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-	orderId: { type: mongoose.Schema.Types.ObjectId, ref: "Order" },
-	reason: { type: String },
-	status: { type: String, enum: ["cancelled", "pending", "non-cancelled"], default: "pending" }
-}, { timestamps: true })
+const mongoose = require("mongoose");
+const Cancellation = new mongoose.Schema(
+  {
+    userid: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    orderId: { type: mongoose.Schema.Types.ObjectId, ref: "Order" },
+    reason: { type: String },
+    status: {
+      type: String,
+      enum: ["cancelled", "pending", "non-cancelled"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Cancellation", Cancellation)
+module.exports = mongoose.model("Cancellation", Cancellation);
